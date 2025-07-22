0b091cbe61b9d8d7c300af54bd7e1d207c264c36
pettylandd <96563286+pettylandd@users.noreply.github.com>
2025-07-22 00:30:38 -0400

    index.js

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -2,5 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// Pass the API URL as a prop to App
+const apiUrl = process.env.REACT_APP_SRTLA_API || "http://localhost:8080";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<App apiUrl={apiUrl} />);
